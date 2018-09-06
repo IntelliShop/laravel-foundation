755bfa0a3b3808b47c7658209216539ff708565c
vladimir.reznichenko <vladimir.reznichenko@intellishop.ag>
2018-09-06 12:21:42 +0200

    [#1] Increased test coverage.

--- tests/IntelliShop/LaravelFoundation/AuthenticationServiceProviderTest.php
+++ tests/IntelliShop/LaravelFoundation/AuthenticationServiceProviderTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace IntelliShop\LaravelFoundation;
 
 use Illuminate\Foundation\Application;
+use IntelliShop\LaravelFoundation\Application\Console\PassportInstallCommand;
 use IntelliShop\LaravelFoundation\Application\Entities\Passport\AuthCode;
 use IntelliShop\LaravelFoundation\Application\Entities\Passport\Client;
 use IntelliShop\LaravelFoundation\Application\Entities\Passport\PersonalAccessClient;
@@ -27,8 +28,8 @@ final class AuthenticationServiceProviderTest extends TestCase
             ->setConstructorArgs([$application])
             ->setMethods(['loadRoutesFrom', 'publishes'])
             ->getMock();
-        $provider->expects($this->once())->method('loadRoutesFrom')->willReturn(null);
-        $provider->expects($this->once())->method('publishes')->willReturn(null);
+        $provider->expects($this->once())->method('loadRoutesFrom');
+        $provider->expects($this->once())->method('publishes');
 
         $provider->boot();
 
@@ -36,5 +37,29 @@ final class AuthenticationServiceProviderTest extends TestCase
         $this->assertSame(Client::class, Passport::clientModel());
         $this->assertSame(AuthCode::class, Passport::authCodeModel());
         $this->assertSame(PersonalAccessClient::class, Passport::personalAccessClientModel());
+    }
+
+    /**
+     * @covers \IntelliShop\LaravelFoundation\AuthenticationServiceProvider::<public>
+     */
+    public function testRegister(): void
+    {
+        $application = $this->getMockBuilder(Application::class)->disableOriginalConstructor()->getMock();
+        $application
+            ->expects($this->exactly(2))
+            ->method('make')
+            ->willReturnCallback(function (string $what): object {
+                return $this->getMockBuilder($what)->disableOriginalConstructor()->getMock();
+            });
+        $application
+            ->expects($this->once())
+            ->method('singleton')
+            ->willReturnCallback(function (string $what, \Closure $factory) use ($application): object {
+                $this->assertSame($what, PassportInstallCommand::class);
+
+                return $factory($application);
+            });
+
+        (new AuthenticationServiceProvider($application))->register();
     }
 }
